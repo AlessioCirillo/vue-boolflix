495f64eccc9aeb67a0bacade5ec69e0a4f58b982
alessio <alessio.cirillo.1995@gmail.com>
2020-11-25 12:56:05 +0100

    print list

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: '#app',
+    
+    data:{
+        movies:[],
+        searchBar:''
+    },
+
+    methods:{
+        search(){
+            axios.get('https://api.themoviedb.org/3/search/movie?',{
+                params:{
+                    api_key: 'cbadb2020a9a1deec5f8094ca2623fe0',
+                    query: this.searchBar,
+                    language: 'it_IT'
+                }
+            })
+            .then(result => {
+                // handle success
+                console.log(result);
+                this.movies = result.data.results
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
+            })
+        }
+    }
+});
